1788dcef4ae5057d459ec1719b9424f95335ed3f
Ruma Das <ruma@neo-analytics.com,au>
2023-06-12 00:42:54 +0930

    "adding controller  to project"

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,17 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-// import products from "../data/products.js";
-import Product from "../models/productModel.js";
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productControllers.js";
+// router.get(
+//   "/",
+//   asyncHandler(async (req, res) => {
+//     const products = await Product.find({});
+//     res.json(products);
+//   })
+// );
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    }
-    res.status(404);
-    throw new Error("Resource not found");
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 export default router;
