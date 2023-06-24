304795c5a7160e1178fb75f8d58af181efffbeea
Ruma Das <ruma@neo-analytics.com,au>
2023-06-24 22:55:28 +0930

    Adding user routes and user controllers

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,20 +1,22 @@
-const notFound = (req, res, nex) => {
-  const error = new Error(`Not Found - ${req.orginalURL}`);
-  res.ststus(404);
-  nextTick(error);
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
-  //check for Mongoose bad ObjectId
+
+  // If Mongoose not found error, set to 404 and change message
   if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = `Resource not found`;
     statusCode = 404;
+    message = "Resource not found";
   }
+
   res.status(statusCode).json({
-    message,
-    stack: process.env.NODE_ENV === "production" ? "0" : err.stack,
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const port = process.env.PORT || 5000;
 connectDB(); //connect to MongoDB
 const app = express();
-
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+///
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
-app.use(notFound);
-app.use(errorHandler);
+// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
+
 // app.get("/api/products", productRoutes);
 // app.get("/api/products", (req, res) => {
 //   res.json(products);
@@ -25,4 +31,6 @@ app.use(errorHandler);
 //   const product = products.find((p) => p._id === req.params.id);
 //   res.json(product);
 // });
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
